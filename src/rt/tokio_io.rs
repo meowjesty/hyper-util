ae51362986cdfd93caf723f990ba5a081aea3a2e
meowjesty <meowjesty_nyan@outlook.com>
2023-08-30 14:27:52 -0300

    Update tokio_io runtime stuff with the hyper mod in benches.

--- src/rt/tokio_io.rs
+++ src/rt/tokio_io.rs
@@ -1,15 +1,96 @@
 #![allow(dead_code)]
-//! Tokio IO integration for hyper
+//! Various runtimes for hyper
 use std::{
     pin::Pin,
     task::{Context, Poll},
+    time::{Duration, Instant},
 };
 
+use futures_util::Future;
+use hyper::rt::{Sleep, Timer};
 use pin_project_lite::pin_project;
 
+#[derive(Clone)]
+/// An Executor that uses the tokio runtime.
+pub struct TokioExecutor;
+
+impl<F> hyper::rt::Executor<F> for TokioExecutor
+where
+    F: std::future::Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    fn execute(&self, fut: F) {
+        tokio::task::spawn(fut);
+    }
+}
+
+/// A Timer that uses the tokio runtime.
+
+#[derive(Clone, Debug)]
+pub struct TokioTimer;
+
+impl Timer for TokioTimer {
+    fn sleep(&self, duration: Duration) -> Pin<Box<dyn Sleep>> {
+        Box::pin(TokioSleep {
+            inner: tokio::time::sleep(duration),
+        })
+    }
+
+    fn sleep_until(&self, deadline: Instant) -> Pin<Box<dyn Sleep>> {
+        Box::pin(TokioSleep {
+            inner: tokio::time::sleep_until(deadline.into()),
+        })
+    }
+
+    fn reset(&self, sleep: &mut Pin<Box<dyn Sleep>>, new_deadline: Instant) {
+        if let Some(sleep) = sleep.as_mut().downcast_mut_pin::<TokioSleep>() {
+            sleep.reset(new_deadline.into())
+        }
+    }
+}
+
+struct TokioTimeout<T> {
+    inner: Pin<Box<tokio::time::Timeout<T>>>,
+}
+
+impl<T> Future for TokioTimeout<T>
+where
+    T: Future,
+{
+    type Output = Result<T::Output, tokio::time::error::Elapsed>;
+
+    fn poll(mut self: Pin<&mut Self>, context: &mut Context<'_>) -> Poll<Self::Output> {
+        self.inner.as_mut().poll(context)
+    }
+}
+
+// Use TokioSleep to get tokio::time::Sleep to implement Unpin.
+// see https://docs.rs/tokio/latest/tokio/time/struct.Sleep.html
 pin_project! {
-    /// A wrapping implementing hyper IO traits for a type that
-    /// implements Tokio's IO traits.
+    pub(crate) struct TokioSleep {
+        #[pin]
+        pub(crate) inner: tokio::time::Sleep,
+    }
+}
+
+impl Future for TokioSleep {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.project().inner.poll(cx)
+    }
+}
+
+impl Sleep for TokioSleep {}
+
+impl TokioSleep {
+    pub fn reset(self: Pin<&mut Self>, deadline: Instant) {
+        self.project().inner.as_mut().reset(deadline.into());
+    }
+}
+
+pin_project! {
+    /// Nice struct
     #[derive(Debug)]
     pub struct TokioIo<T> {
         #[pin]
@@ -18,18 +99,13 @@ pin_project! {
 }
 
 impl<T> TokioIo<T> {
-    /// Wrap a type implementing Tokio's IO traits.
+    /// New creates stuff
     pub fn new(inner: T) -> Self {
         Self { inner }
     }
 
-    /// Borrow the inner type.
-    pub fn inner(&self) -> &T {
-        &self.inner
-    }
-
-    /// Consume this wrapper and get the inner type.
-    pub fn into_inner(self) -> T {
+    /// Inner stuff
+    pub fn inner(self) -> T {
         self.inner
     }
 }
